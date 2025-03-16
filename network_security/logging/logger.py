363ed8655797331826e998b1669b2b66ce0cbcf2
Priyanshu Verma <hellopriyanshu12@gmail.com>
2025-03-16 17:00:53 +0530

    ETL completed

---
+++ network_security/logging/logger.py
@@ -0,0 +1,18 @@
+import logging
+import os
+from datetime import datetime
+
+
+Log_format = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+logs_path=os.path.join(os.getcwd(),"logs",Log_format)
+
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path,Log_format)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
